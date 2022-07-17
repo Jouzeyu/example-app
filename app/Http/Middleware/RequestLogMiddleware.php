b5e948cbfb902f76f1a74793d6af33a8054496ed
Jouzeyu <jouzeyu@outlook.com>
2022-07-17 22:37:44 +0800

    feat(辅助开发工具):通过中间件的方式将请求记录记录到log日志中。

---
+++ app/Http/Middleware/RequestLogMiddleware.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Jobs\LogJob;
+use Closure;
+use Illuminate\Http\Request;
+
+class RequestLogMiddleware
+{
+    /**
+     * @param Request $request
+     * @param Closure $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next): mixed
+    {
+        $time1 = microtime(true);
+        $response = $next($request);
+        $time2 = microtime(true);
+        //通过中间件 记录操作记录
+        LogJob::dispatch([
+            'url' => $request->path(),
+            'request' => $request->all(),
+            'response' => $next($request),
+            'time' => sprintf("%.4f", ($time2 - $time1)) . '秒',
+            'method' => $request->method(),
+        ]);
+        return $response;
+    }
+}
---
+++ app/Jobs/LogJob.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
+
+class LogJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public $data;
+
+    public string $RunJob = __CLASS__;
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle(): void
+    {
+        Log::info('--------------------------请求日志队列开始--------------------------');
+        Log::info('队列名称:'.$this->RunJob);
+        Log::info('请求地址:'.$this->data['url']);
+        Log::info('请求方式:'.$this->data['method']);
+        Log::info('请求参数:'.json_encode($this->data['request'] ,JSON_UNESCAPED_UNICODE));
+        Log::info('返回结果:'.json_encode($this->data['response'] ,JSON_UNESCAPED_UNICODE));
+        Log::info('运行时长:'.$this->data['time']);
+        Log::info('--------------------------请求日志队列结束--------------------------');
+        Log::info('');
+    }
+}
